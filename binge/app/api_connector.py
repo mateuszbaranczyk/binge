6899c51827ba08c149c6ebb60e6ae40b871660d4
kundziorro <grzybek2413@gmail.com>
2022-11-11 16:33:14 +0100

    moved request to separate func

--- binge/app/api_connector.py
+++ binge/app/api_connector.py
@@ -1,10 +1,8 @@
 import os
 from ast import literal_eval
+from typing import Tuple
 
 import requests
-
-# TODO api errors handling
-
 
 class Requester:
     def __init__(self):
@@ -18,7 +16,7 @@ class Requester:
         best_match = literal_eval(response)["results"][0]
         return best_match["id"]
 
-    def get_title_data(self, title_id: str) -> Tuple[str, str, str]:
+    def get_title_data(self, title_id: str) -> Tuple[int, str, str]:
         query = "Title"
         url = f"{self.url}/{query}/{self.api_key}/{title_id}"
         response = requests.get(url=url).text
@@ -26,4 +24,13 @@ class Requester:
         num_seasons = response["tvSeriesInfo"]["seasons"][-1]
         full_title = response["fullTitle"]
         image = response["image"]
-        return (num_seasons, full_title, image)
+        return num_seasons, full_title, image
+
+    def get_title_duration(self, title_id: str, num_seasons: int):
+        pass
+
+    def _make_request(self, query: str, query_params: str) -> dict:
+        path = f"{self.url}/{query}/{self.api_key}/{query_params}"
+        response = requests.get(path)
+        assert response.status_code == 200
+        return literal_eval(response.text)
