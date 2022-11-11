eafefc1d31645aa4f971591e4b0250a2b8d5ea02
kundziorro <grzybek2413@gmail.com>
2022-11-11 16:32:38 +0100

    test for make request

--- binge/tests/test_api_connector.py
+++ binge/tests/test_api_connector.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+from unittest import TestCase
 
 from binge.app.api_connector import Requester
 from binge.tests.testing_responses import SearchSeries_response, Title_response
@@ -22,3 +23,26 @@ def test_get_title_data():
             "Lost (TV Series 2004–2010)",
             "https://m.media-amazon.com/images/M/MV5BNzhlY2E5NDUtYjJjYy00ODg3LWFkZWQtYTVmMzU4ZWZmOWJkXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_Ratio0.6762_AL_.jpg",
         )
+
+
+def test_get_title_duration():
+    pass
+
+def test_make_request():
+    with patch("requests.get") as mocked_request:
+        mocked_request.return_value.text = '{"test": "data"}'
+        mocked_request.return_value.status_code = 200
+        requester = Requester()
+        result = requester._make_request("query", "query_params")
+        result = {"test": "data"}
+
+def test_make_request_with_api_error():
+    with patch("requests.get") as mocked_request:
+        mocked_request.return_value.status_code = 404
+        requester = Requester()
+        try:
+            result = requester._make_request("query", "query_params")
+        except AssertionError:
+            pass
+
+        
