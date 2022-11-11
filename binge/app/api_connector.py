a2a64fa13f44c778de79d2b50cecf37371ba60c7
kundziorro <grzybek2413@gmail.com>
2022-11-11 12:34:18 +0100

    test for get title data

--- binge/app/api_connector.py
+++ binge/app/api_connector.py
@@ -8,7 +8,7 @@ import requests
 
 class Requester:
     def __init__(self):
-        self.url = "https://imdb-api.com/en/API/"
+        self.url = "https://imdb-api.com/en/API"
         self.api_key = os.getenv("IMDB_API_KEY")
 
     def get_id_by_phrase(self, phrase: str) -> str:
@@ -18,5 +18,12 @@ class Requester:
         best_match = literal_eval(response)["results"][0]
         return best_match["id"]
 
-    def get_single_episode(self):
-        pass
+    def get_title_data(self, title_id: str):
+        query = "Title"
+        url = f"{self.url}/{query}/{self.api_key}/{title_id}"
+        response = requests.get(url=url).text
+        response = literal_eval(response)
+        num_seasons = response["tvSeriesInfo"]["seasons"][-1]
+        full_title = response["fullTitle"]
+        image = response["image"]
+        return num_seasons, full_title, image
--- binge/tests/test_api_connector.py
+++ binge/tests/test_api_connector.py
@@ -1,12 +1,24 @@
 from unittest.mock import patch
 
 from binge.app.api_connector import Requester
-from binge.tests.testing_responses import SearchSeries_api_response
+from binge.tests.testing_responses import SearchSeries_response, Title_response
 
 
 def test_get_id_by_phrase():
     with patch("requests.get") as mocked_request:
-        mocked_request.return_value.text = str(SearchSeries_api_response)
+        mocked_request.return_value.text = str(SearchSeries_response)
         requester = Requester()
         result = requester.get_id_by_phrase("lost")
         assert result == "tt0411008"
+
+
+def test_get_title_data():
+    with patch("requests.get") as mocked_request:
+        mocked_request.return_value.text = str(Title_response)
+        requester = Requester()
+        result = requester.get_title_data("tt0411008")
+        assert result == (
+            "6",
+            "Lost (TV Series 2004–2010)",
+            "https://m.media-amazon.com/images/M/MV5BNzhlY2E5NDUtYjJjYy00ODg3LWFkZWQtYTVmMzU4ZWZmOWJkXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_Ratio0.6762_AL_.jpg",
+        )
