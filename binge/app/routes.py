5e54a058df336aa7154aab3a61e8b217eb972d6a
kundziorro <grzybek2413@gmail.com>
2022-11-13 12:57:57 +0100

    created base template

--- binge/app/routes.py
+++ binge/app/routes.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from api_connector import Requester
 
 app = Flask(__name__)
+requester = Requester()
+
 
 
 @app.route("/")
 def main_page():
+    return render_template("index.html")
+
+@app.route("/<title>")
+def show_title_page(title: str):
     pass
