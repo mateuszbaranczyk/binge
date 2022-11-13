7f50aa2bffe72d1650e965e60725f0630dc5c5eb
kundziorro <grzybek2413@gmail.com>
2022-11-13 15:26:50 +0100

    fix form

--- binge/app/forms.py
+++ binge/app/forms.py
@@ -3,5 +3,4 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Length, DataRequired
 
 class QueryForm(FlaskForm):
-    title = StringField("Title", validators=[Length(min=3), DataRequired()])
-    submit = SubmitField("Find it!")
+    title = StringField("Title", validators=[DataRequired()])
--- binge/app/routes.py
+++ binge/app/routes.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from api_connector import Requester
 from forms import QueryForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "twojastara"
+app.config["SECRET_KEY"] = "twojastara"
 requester = Requester()
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
     form = QueryForm()
-    return render_template("find.html", title="Binge", form=form)
+    if form.validate_on_submit():
+        title = form.title.data
+        return redirect(url_for("title_page", title=title))
+    return render_template('find.html', form=form)
 
 
-@app.route("/title")
+@app.route('/title', methods=['GET', 'POST'])
 def title_page():
     return render_template("title.html")
