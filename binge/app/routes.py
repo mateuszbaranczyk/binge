987c471f93af6b189773efa2fc90180ff5262b77
kundziorro <grzybek2413@gmail.com>
2022-11-13 18:32:57 +0100

    created endpoint for title site

--- binge/app/routes.py
+++ binge/app/routes.py
@@ -7,19 +7,31 @@ app.config["SECRET_KEY"] = "twojastara"
 requester = Requester()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def main_page():
     form = QueryForm()
     if form.validate_on_submit():
         title = form.title.data
         return redirect(url_for("title_page", title=title))
-    return render_template('find.html', form=form)
+    return render_template("find.html", form=form)
 
 
-@app.route('/title', methods=['GET', 'POST'])
+@app.route("/title", methods=["GET", "POST"])
 def title_page():
-    title = request.args.get('title')
-    atributes = {"title": title}
-    return render_template("title.html", atributes=atributes)
+    title = request.args.get("title")
+    # title_id = requester.get_id_by_phrase(phrase=title)
+    # response = requester.get_title_data(title_id)
+    response = (
+        "8",
+        "Game of Thrones (TV Series 2011–2019)",
+        "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_Ratio0.7331_AL_.jpg",
+        None,
+    )
+    title_data = {
+        "title": response[1],
+        "image": response[2],
+        "seasons": response[0],
+        "description": "some dummy desc",
+    }
 
-
+    return render_template("title.html", title_data=title_data)
