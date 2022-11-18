09c707786dda2c66a7e02340bb9697adb53b48c4
kundziorro <grzybek2413@gmail.com>
2022-11-18 16:07:49 +0100

    created form and answer template

--- binge/app/forms.py
+++ binge/app/forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class QueryForm(FlaskForm):
     title = StringField(
         "Title", validators=[DataRequired()], render_kw={"placeholder": "Teletubbies"}
     )
+
+
+class PeroidForm(FlaskForm):
+    duration = StringField(
+        "Requested duration",
+        validators=[DataRequired()],
+        render_kw={"placeholder": "7"},
+        default="7",
+    )
+    peroid = SelectField("Peroid", choices=["days", "weeks", "months"], default="days")
+    go = SubmitField()
--- binge/app/routes.py
+++ binge/app/routes.py
@@ -1,10 +1,17 @@
 from api_connector import Requester
-from flask import Flask, flash, redirect, render_template, request, url_for
-from forms import QueryForm
+from flask import Flask, redirect, render_template, request, url_for
+from forms import PeroidForm, QueryForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "twojastara"
 requester = Requester()
+
+fake_data = (
+    "8",
+    "Game of Thrones (TV Series 2011–2019)",
+    "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_Ratio0.7331_AL_.jpg",
+    None,
+)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,27 +23,34 @@ def main_page():
     return render_template("find.html", form=form)
 
 
-@app.route("/about", methods=["GET"])
-def about():
-    return render_template("about.html")
-
-
 @app.route("/title", methods=["GET", "POST"])
 def title_page():
+    form = PeroidForm()
     # title = request.args.get("title")
     # title_id = requester.get_id_by_phrase(phrase=title)
     # response = requester.get_title_data(title_id)
-    response = (
-        "8",
-        "Game of Thrones (TV Series 2011–2019)",
-        "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_Ratio0.7331_AL_.jpg",
-        None,
-    )
+
     title_data = {
-        "title": response[1],
-        "image": response[2],
-        "seasons": response[0],
+        "title": fake_data[1],
+        "image": fake_data[2],
+        "seasons": fake_data[0],
         "description": "some dummy desc",
     }
+    if form.validate_on_submit():
+        peroid = form.peroid.data
+        duration = form.duration.data
+        return redirect(url_for("answer", peroid=peroid, duration=duration))
+    return render_template("title.html", title_data=title_data, form=form)
 
-    return render_template("title.html", title_data=title_data)
+
+@app.route("/answer", methods=["GET", "POST"])
+def answer():
+    peroid = request.args.get("peroid")
+    duration = request.args.get("duration")
+
+    return render_template("answer.html", peroid=peroid, duration=duration)
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
