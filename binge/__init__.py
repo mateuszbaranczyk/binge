01b7bd999483e7e84051315db53a473597c392f9
kundziorro <grzybek2413@gmail.com>
2022-11-20 17:01:29 +0100

    init flask app

--- binge/__init__.py
+++ binge/__init__.py
@@ -1,25 +1,24 @@
-import secrets
 import os
+import secrets
 
-from flask import Flask, session
+from flask import Flask
 
 from flask_session import Session
-import secrets
 
-#enviroment variables 
+# enviroment variables
 # FLASK_APP=binge
-# FLASK_ENV=development
+# FLASK_DEBUG=True
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=secrets.token_hex(16),
         SESSION_PERMANENT=False,
-        SESSION_TYPE="filesystem"
+        SESSION_TYPE="filesystem",
     )
-
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -29,9 +28,8 @@ def create_app(test_config=None):
     except OSError:
         print("OSError")
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import routes
 
+    app.register_blueprint(routes.bp)
+    
     return app
