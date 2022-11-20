153a65cba8dd7924d1808df758b8a8c7cb753cba
kundziorro <grzybek2413@gmail.com>
2022-11-20 17:37:24 +0100

    session init in app

--- binge/__init__.py
+++ binge/__init__.py
@@ -14,15 +14,18 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=secrets.token_hex(16),
-        SESSION_PERMANENT=False,
-        SESSION_TYPE="filesystem",
     )
+    
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    _session = Session()
+    _session.init_app(app)
+
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError as err:
