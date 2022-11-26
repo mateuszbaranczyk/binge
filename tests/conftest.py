c53dec772055dc4a6bb33c47a04a83e09424b30a
kundziorro <grzybek2413@gmail.com>
2022-11-26 21:47:03 +0100

    pyteste skipof

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,3 +44,13 @@ def pytest_cmdline_preparse(config, args):
         return
 
     _pytest.skipping.skip = no_skip
+
+#TODO try to use it
+# import pytest
+
+# old_skipif = pytest.mark.skipif
+
+# def custom_skipif(*args, **kwargs):
+#     return old_skipif(False, reason='disabling skipif')
+
+# pytest.mark.skipif = custom_skipif
