367243659e838db4462507936e0df18d69881f57
kundziorro <grzybek2413@gmail.com>
2022-11-26 21:16:28 +0100

    tests cleaning

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,11 +4,12 @@ import pytest
 
 from binge import create_app
 from binge.forms import PeroidForm
-from binge.routes import (
-    _check_if_can_be_binged,
-    _redirect_to_answer_page,
-    _redirect_to_title_page,
-)
+
+# black/isort conflict
+# fmt: off
+from binge.routes import (_check_if_can_be_binged, _redirect_to_answer_page,
+                          _redirect_to_title_page)
+# fmt: on
 from tests.testing_api_responses import title_data
 from tests.testing_endpoint_responses import home_form_btn, home_form_field
 
@@ -28,15 +29,9 @@ def test_redirect_to_title_page(get_id_by_phrase, get_title_data, client):
     with client:
         client.get("/")
         form = _create_query_form(title="test")
-        response = _redirect_to_title_page(form=form)
-    assert response.status_code == 302
-    assert response.location == "/title"
-
-
-def _create_query_form(title) -> "patched_form":
-    patched_form = MagicMock()
-    patched_form.return_value.title.data = title
-    return patched_form
+        redirect = _redirect_to_title_page(form=form)
+    assert redirect.status_code == 302
+    assert redirect.location == "/title"
 
 
 def test_render_title_page(client, session):
@@ -57,22 +52,14 @@ def test_render_answer_page(client, session):
 
 @patch("binge.api_connector.requester.get_title_duration")
 def test_redirect_to_answer_page(get_title_duration, client, session):
-    title_data = {"id": "test_id", "seasons": "1"}
     session(title_data=title_data)
     get_title_duration.return_value = 12
     with client:
         client.get("/title")
         form = _create_peroid_form(peroid="1", duration="24")
-        response = _redirect_to_answer_page(form, title_data)
-    assert response.status_code == 302
-    assert response.location == "/answer"
-
-
-def _create_peroid_form(peroid: str, duration: str) -> "patched_form":
-    patched_form = MagicMock()
-    patched_form.return_value.peroid.data = peroid
-    patched_form.return_value.duration.data = duration
-    return patched_form
+        redirect = _redirect_to_answer_page(form, title_data)
+    assert redirect.status_code == 302
+    assert redirect.location == "/answer"
 
 
 @pytest.mark.parametrize(
@@ -86,3 +73,16 @@ def _create_peroid_form(peroid: str, duration: str) -> "patched_form":
 def test__check_if_can_be_binged(peroid, duration, title_duration, expected_result):
     result = _check_if_can_be_binged(peroid, duration, title_duration)
     assert result == expected_result
+
+
+def _create_query_form(title) -> "mocked_form":
+    mocked_form = MagicMock()
+    mocked_form.return_value.title.data = title
+    return mocked_form
+
+
+def _create_peroid_form(peroid: str, duration: str) -> "mocked_form":
+    mocked_form = MagicMock()
+    mocked_form.return_value.peroid.data = peroid
+    mocked_form.return_value.duration.data = duration
+    return mocked_form
