5e0658edbb63cb84c75e4b62483931831414e329
kundziorro <grzybek2413@gmail.com>
2022-12-04 17:21:40 +0100

    test db

--- binge/database.py
+++ binge/database.py
@@ -10,3 +10,10 @@ class TitleData(db.Model):
     image = db.Column(db.String)
     seasons = db.Column(db.String)
     duration = db.Column(db.String, nullable=True)
+
+class Operations:
+    def __init__(self):
+        self.db = SQLAlchemy()
+
+    def add_title_data_to_db(self, title_data: dict) -> None:
+        pass
