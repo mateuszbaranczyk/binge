bfd25d69fc4d8b1517d2f81a0b1fe3699bb34007
kundziorro <grzybek2413@gmail.com>
2022-12-04 15:18:54 +0100

    mod test resources

--- tests/testing_endpoint_responses.py
+++ tests/testing_endpoint_responses.py
@@ -1,2 +1,2 @@
-home_form_field = b'<input class="form-control" id="title" name="title" placeholder="Teletubbies" required size="20" type="text" value="">'
-home_form_btn = b'<input class="btn btn-secondary" type="submit" value="Find it!">'
+home_form_field = '<input class="form-control" id="form-title" name="form-title" placeholder="Teletubbies" required size="20" type="text" value="">'
+home_form_btn = '<input class="btn btn-secondary" id="form-find_it" name="form-find_it" type="submit" value="Find It">'
