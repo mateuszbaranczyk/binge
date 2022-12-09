40ab354e128f9637c56330c65ea37da87fa8a08a
Jacek Płocharczyk <jacek.plocharczyk@protonmail.com>
2022-12-09 20:10:50 +0100

    untested db operations

--- binge/database.py
+++ binge/database.py
@@ -11,9 +11,34 @@ class TitleData(db.Model):
     seasons = db.Column(db.String)
     duration = db.Column(db.String, nullable=True)
 
+
 class Operations:
     def __init__(self):
-        self.db = SQLAlchemy()
+        pass
 
-    def add_title_data_to_db(self, title_data: dict) -> None:
+    def save_title(self, title_data: dict) -> None:
+        data_to_commit = TitleData(
+            id=title_data["id"],
+            title=title_data["title"],
+            descriptrion=title_data["description"],
+            image=title_data["image"],
+            seasons=title_data["seasons"],
+            duration=title_data["duration"],
+        )
+        db.add(data_to_commit)
+        db.commit()
+        # TODO logging
+        return None
+
+    def get_title(self, id: str) -> dict:
+        title = db.get(TitleData, id)
+        db.get()
+        return title.__dict__
+
+    def update_title(self, id: str, **kwargs):
+        title = db.get_or_404(TitleData, id)
+        for key, value in kwargs:
+            setattr(title, key, value)
+
+    def genearate_thumbnail(self):
         pass
