41a3ffcd56ab8bbd9906d5ca1d4c21ed5388af3d
kundziorro <grzybek2413@gmail.com>
2022-12-10 16:52:19 +0100

    test and config for db

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,12 +1,20 @@
-from binge import database
+from binge import db
 from tests.testing_api_responses import title_data
-from flask_sqlalchemy import SQLAlchemy
+from binge import database
+import pytest
 
-db = SQLAlchemy()
 operations = database.Operations()
 
+
+@pytest.fixture(autouse=True)
+def setup_andteardown(app_context):
+    db.create_all()
+    yield
+    db.session.remove()
+    db.drop_all()
 
 def test_add_title_data_to_db(app_context):
     operations.save_title(title_data=title_data)
     result = db.get_or_404(database.TitleData, title_data["id"])
-    assert result is not None
+    assert result.title == title_data["title"]
+
